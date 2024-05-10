716ac754ef8cbb1eac5fee8d036ba2a8b566fb27
Xennia Contreras <Xenbydesign@gmail.com>
2024-05-09 19:46:00 -0700

    darkmode working yelp still broken

--- server/routes/strains.py
+++ server/routes/strains.py
@@ -1,26 +1,11 @@
-from . import Resource, request, db, Strain, strains_schema
+from . import Resource, request, db, Strain, strains_schema, jwt_required
 
 
 class Strains(Resource):
+    @jwt_required()
     def get(self):
         try:
             strain = strains_schema.dump(Strain.query)
             return strain, 200
         except Exception as e:
             return str(e), 400
-
-    def delete(self, id):
-        db.session.delete(Strain.query.get(id))
-        db.session.commit()
-        return "", 204
-
-    def post(self):
-        try:
-            data = request.get_json()
-            new_bud = strains_schema.load(data)
-            db.session.add(new_bud)
-            db.session.commit()
-            return strains_schema.dump(new_bud), 201
-        except Exception as e:
-            db.session.rollback()
-            return {"message": str(e)}, 422
